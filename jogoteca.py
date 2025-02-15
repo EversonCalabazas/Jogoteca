b33511750b36c73343384ebd3b67b4105a2e0955
EversonCalabazas <everson.esc@gmail.com>
2025-02-15 12:24:33 -0400

    adicionando Classe Jogo

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    lista = ['Tetris','skyrim','crash']
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slack', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
     return render_template("listas.html", titulo="Jogos", jogos=lista)
 
 
