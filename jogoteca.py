ac995292887e034be51e4ba9bbb899202b34063e
EversonCalabazas <everson.esc@gmail.com>
2025-02-17 20:58:39 -0400

    Nova pagina de criar jogos e exibir jogos novos

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,15 +6,28 @@ class Jogo:
         self.categoria=categoria
         self.console=console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slack', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]        
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slack', 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
+def ola():
     return render_template("listas.html", titulo="Jogos", jogos=lista)
 
+@app.route("/novo")
+def novo():
+    return render_template("novo.html", titulo="Novo Jogo")
 
-app.run()
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('listas.html', titulo='Jogos', jogos=lista)
+
+app.run(debug=True)
