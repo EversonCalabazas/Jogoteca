f583b63f6998b98890bc85ad0ca6e14ed13d17b0
EversonCalabazas <everson.esc@gmail.com>
2025-02-17 23:47:53 -0400

    adicionado rota logout

--- jogoteca.py
+++ jogoteca.py
@@ -45,5 +45,11 @@ def autenticar():
     else:
         flash('Usuário não logado!')
         return redirect('/login')
+    
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect('/')    
 
 app.run(debug=True)
